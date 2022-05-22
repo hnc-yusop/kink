7baff9fd5508737e22cd61df27fdd7ce16fe2c03
김유섭 <yusop@hancom.com>
2022-05-22 11:15:45 +0900

    Refine

--- docker-images/kind-cluster/entrypoint-wrapper.sh
+++ docker-images/kind-cluster/entrypoint-wrapper.sh
@@ -22,17 +22,17 @@ CERT_SANS+=(127.0.0.1)
 
 UNIQUE_CERT_SANS=($(echo "${CERT_SANS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 
-#for hostname in "${UNIQUE_CERT_SANS[@]}"; do
-#cat <<EOF >> kind-config.yaml
-#  - group: kubeadm.k8s.io
-#    version: v1beta2
-#    kind: ClusterConfiguration
-#    patch: |
-#      - op: add
-#        path: /apiServer/certSANs/-
-#        value: ${hostname}
-#EOF
-#done
+for hostname in "${UNIQUE_CERT_SANS[@]}"; do
+cat <<EOF >> kind-config.yaml
+  - group: kubeadm.k8s.io
+    version: v1beta2
+    kind: ClusterConfiguration
+    patch: |
+      - op: add
+        path: /apiServer/certSANs/-
+        value: ${hostname}
+EOF
+done
 
 kind create cluster --name=${KIND_CLUSTER_NAME:-""} --config=kind-config.yaml --image=${KIND_NODE_IMAGE-"registry.trendyol.com/platform/base/image/kind-node:v1.21.2"} --wait=900s
 

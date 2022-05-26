2d1471cbffeca816745e53e75c6be0a5adb875d2
김유섭 <yusop@hancom.com>
2022-05-26 22:05:07 +0900

    asljdfl

--- docker-images/kind-cluster/entrypoint-wrapper.sh
+++ docker-images/kind-cluster/entrypoint-wrapper.sh
@@ -14,25 +14,25 @@ echo "Setting up KIND cluster"
 API_SERVER_ADDRESS=${API_SERVER_ADDRESS:-"127.0.0.1"}
 sed -i "s/apiServerAddress:$/apiServerAddress: ${API_SERVER_ADDRESS}/" kind-config.yaml
 
-#CERT_SANS=(${CERT_SANS:-""})
-#CERT_SANS+=(${API_SERVER_ADDRESS})
-#CERT_SANS+=($(hostname -i))
-#CERT_SANS+=(localhost)
-#CERT_SANS+=(127.0.0.1)
+CERT_SANS=(${CERT_SANS:-""})
+CERT_SANS+=(${API_SERVER_ADDRESS})
+CERT_SANS+=($(hostname -i))
+CERT_SANS+=(localhost)
+CERT_SANS+=(127.0.0.1)
 
-#UNIQUE_CERT_SANS=($(echo "${CERT_SANS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+UNIQUE_CERT_SANS=($(echo "${CERT_SANS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 
-#for hostname in "${UNIQUE_CERT_SANS[@]}"; do
-#cat <<EOF >> kind-config.yaml
-#  - group: kubeadm.k8s.io
-#    version: v1beta2
-#    kind: ClusterConfiguration
-#    patch: |
-#      - op: add
-#        path: /apiServer/certSANs/-
-#        value: ${hostname}
-#EOF
-#done
+for hostname in "${UNIQUE_CERT_SANS[@]}"; do
+cat <<EOF >> kind-config.yaml
+  - group: kubeadm.k8s.io
+    version: v1beta2
+    kind: ClusterConfiguration
+    patch: |
+      - op: add
+        path: /apiServer/certSANs/-
+        value: ${hostname}
+EOF
+done
 
 kind create cluster --name=${KIND_CLUSTER_NAME:-""} --config=kind-config.yaml --image=${KIND_NODE_IMAGE-"registry.trendyol.com/platform/base/image/kind-node:v1.21.2"} --wait=900s
 

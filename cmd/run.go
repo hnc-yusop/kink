0acf532ac29eedf07ecadcdf7920b28eab6f45a3
김유섭 <yusop@hancom.com>
2022-05-26 09:43:03 +0900

    lajsdflkjladf

--- cmd/run.go
+++ cmd/run.go
@@ -22,19 +22,19 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
+	//"io/ioutil"
 	"log"
 	"net/url"
 	"os"
 	"os/user"
-	"path/filepath"
+	//"path/filepath"
 	"strings"
 	"time"
 
-	"github.com/ghodss/yaml"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/tools/clientcmd"
-	clientcmdapilatest "k8s.io/client-go/tools/clientcmd/api/latest"
+	//"github.com/ghodss/yaml"
+	//"k8s.io/apimachinery/pkg/runtime"
+	//"k8s.io/client-go/tools/clientcmd"
+	//clientcmdapilatest "k8s.io/client-go/tools/clientcmd/api/latest"
 
 	"github.com/Trendyol/kink/pkg/kubernetes"
 	"github.com/Trendyol/kink/pkg/types"
@@ -42,7 +42,7 @@ import (
 	"github.com/schollz/progressbar/v3"
 	"github.com/spf13/cobra"
 	corev1 "k8s.io/api/core/v1"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	//k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/apimachinery/pkg/util/uuid"
@@ -274,6 +274,7 @@ func NewCmdRun() *cobra.Command {
 			if err != nil {
 				return err
 			}
+			_ = kubeconfig
 
 			hostIP, err := doExec(name, namespace, []string{"sh", "-c", "echo $CERT_SANS"})
 			if err != nil {
@@ -291,6 +292,7 @@ func NewCmdRun() *cobra.Command {
 			kubeconfig = strings.ReplaceAll(kubeconfig, podIP, hostIP)
 			*/
 
+/*
 			serviceClient := client.CoreV1().Services(namespace)
 
 			// Create resource object
@@ -345,9 +347,7 @@ func NewCmdRun() *cobra.Command {
 
 			nodePort := svc.Spec.Ports[0].NodePort
 			_ = nodePort
-			/*
-			kubeconfig = strings.ReplaceAll(kubeconfig, "30001", fmt.Sprint(nodePort))
-			*/
+			//kubeconfig = strings.ReplaceAll(kubeconfig, "30001", fmt.Sprint(nodePort))
 
 			kubeconfigPath := filepath.Join(outputPath, "kubeconfig")
 
@@ -389,7 +389,6 @@ func NewCmdRun() *cobra.Command {
 			if err != nil {
 				return err
 			}
-
 			fmt.Printf(`Thanks for using kink!
 Pod %s and Service %s created successfully!
 
@@ -399,6 +398,7 @@ $ kubectl logs -f %s -n %s
 KUBECONFIG file generated at path '%s'. 
 Start managing your internal KinD cluster by running the following command:
 $ KUBECONFIG=%s kubectl get nodes -o wide`, name, name, name, namespace, kubeconfigPath, kubeconfigPath)
+*/
 			return nil
 		},
 	}
